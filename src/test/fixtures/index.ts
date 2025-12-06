1d507a9baaad9a56bc1f44cb3002fe0f2b7f98b2
vadash1906 <vadash1906@gmail.com>
2025-12-06 19:01:46 +0300

    Update real tests

--- src/test/fixtures/index.ts
+++ src/test/fixtures/index.ts
@@ -26,7 +26,83 @@ export interface TestFixture {
  * Test fixtures - add new test cases here
  */
 export const fixtures: TestFixture[] = [
-  // ADD HERE
+  {
+    name: "Sample 1 - Vinland Majesty",
+    file: "sample1.txt",
+    expectedCharacters: [
+      { name: "Fielding", gender: "male" },
+      { name: "Tennyson", gender: "male" },
+      { name: "Alan", gender: "male" },
+      { name: "Conrad", gender: "male" },
+      { name: "Agnes", gender: "female" },
+      { name: "Willard", gender: "male" },
+      { name: "White", gender: "male" },
+      { name: "Harry", gender: "male" },
+    ],
+    expectedDialogueLines: [
+      { textContains: "Alan, I'm so sorry", speaker: "Tennyson", strict: false },
+      { textContains: "I'll get them", speaker: "Fielding", strict: false },
+      { textContains: "Leave it to the authorities", speaker: "Tennyson", strict: false },
+      { textContains: "There are no bloody authorities", speaker: "Fielding", strict: false },
+      { textContains: "It could still be some kind of private estate", speaker: "Tennyson", strict: false },
+      { textContains: "On the coast", speaker: "Fielding", strict: false },
+      { textContains: "Willard thinks we've gone back in time", speaker: "Fielding", strict: false },
+      { textContains: "We can put the hows and whys", speaker: "Tennyson", strict: false },
+      { textContains: "I'll be there", speaker: "Fielding", strict: false },
+      { textContains: "Sure", speaker: "Conrad", strict: false },
+      { textContains: "Make sure those two are at the meeting", speaker: "Tennyson", strict: false },
+      { textContains: "Do they have any experience", speaker: "Fielding", strict: false },
+      { textContains: "Alan, I know how you feel", speaker: "Tennyson", strict: false },
+      { textContains: "No you bloody don't", speaker: "Fielding", strict: false },
+      { textContains: "I don't intend to leave it up to chance", speaker: "Fielding", strict: false },
+      { textContains: "I can't condone murder", speaker: "Tennyson", strict: false },
+      { textContains: "I'm going, Dave", speaker: "Fielding", strict: false },
+      { textContains: "If necessary", speaker: "Tennyson", strict: false },
+      { textContains: "I don't intend to find out", speaker: "Fielding", strict: false },
+      { textContains: "And we bring them back alive", speaker: "Tennyson", strict: false },
+      { textContains: "That's down to them", speaker: "Fielding", strict: false },
+      { textContains: "go get some rest", speaker: "Tennyson", strict: false },
+    ],
+  },
+  {
+    name: "Sample 2 - Russian Dialogue",
+    file: "sample2.txt",
+    expectedCharacters: [
+      { name: "Мартин", gender: "male" },
+      { name: "Женька", gender: "male" },
+    ],
+    expectedDialogueLines: [
+      { textContains: "Март, чем ты сейчас занимаешься", speaker: "Женька", strict: false },
+      { textContains: "Всякой фигней", speaker: "Мартин", strict: false },
+      { textContains: "Ты ведешь какое-то серьезное дело", speaker: "Женька", strict: false },
+      { textContains: "Заканчиваю", speaker: "Мартин", strict: false },
+      { textContains: "А что осталось не законченным", speaker: "Женька", strict: false },
+      { textContains: "Девочка кое-что успела мне сообщить", speaker: "Мартин", strict: false },
+      { textContains: "Меня расспрашивали о тебе", speaker: "Женька", strict: false },
+      { textContains: "Мент", speaker: "Мартин", strict: false },
+      { textContains: "Госбезопасность", speaker: "Женька", strict: false },
+      { textContains: "Да что им от меня надо", speaker: "Мартин", strict: false },
+      { textContains: "Вот уж не знаю", speaker: "Женька", strict: false },
+    ],
+  },
+  {
+    name: "Sample 3 - Mirian and Lily",
+    file: "sample3.txt",
+    expectedCharacters: [
+      { name: "Mirian", gender: "female" },
+      { name: "Lily", gender: "female" },
+    ],
+    expectedDialogueLines: [
+      { textContains: "Mirian? Are you okay", speaker: "Lily", strict: false },
+      { textContains: "nightmare or something", speaker: "Mirian", strict: false },
+      { textContains: "What’s up", speaker: "Lily", strict: false },
+      { textContains: "There’s a hole", speaker: "Mirian", strict: false },
+      { textContains: "Oh, great", speaker: "Lily", strict: false },
+      { textContains: "No idea", speaker: "Mirian", strict: false },
+      { textContains: "Don’t you have an enchantments exam", speaker: "Lily", strict: false },
+      { textContains: "Yes", speaker: "Mirian", strict: false },
+    ],
+  },
 ];
 
 /**
