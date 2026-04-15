341293c7d14ef8311a70dc2d7486e8762ef32ed2
vadash <vadash@gmail.com>
2026-04-15 19:03:18 +0300

    feat: export NotificationBanner from common index

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Card } from './Card';
 export { Input } from './Input';
+export { NotificationBanner } from './NotificationBanner';
 export { Select } from './Select';
 export { Slider } from './Slider';
 export type { Tab } from './Tabs';
